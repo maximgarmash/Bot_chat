bc141e53cb388f587832b29981dcc4277a14ae94
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-17 19:11:35 +0300

    import modules and add token

--- bot.py
+++ bot.py
@@ -1,1 +1,3 @@
-# add comments
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+updater = Updater(token='ВАШ API КЛЮЧ') # Токен API к Telegram
+dispatcher = updater.dispatcher
