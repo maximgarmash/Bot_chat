0735982cf9379537be00b454e00e3a773727bc5f
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-23 13:14:43 +0300

    add object bot

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import telebot
+import constants
 
-token = "879642920:AAF8LyvzU96NYeE6hygJfjro7ITmglp1aDI"
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(constants.token)
 
-bot.send_message(726853849, "Response test 2")
+# bot.send_message(constants.chat_id, "Response test 2")
+upd = bot.get_updates()
+print(upd)
