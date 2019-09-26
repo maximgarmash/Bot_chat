e03bcadc8db1542150f192f929d3c0cf2ad73908
Maxim Garmash <maxim.garmash@gmail.com>
2019-09-26 19:09:03 +0300

    add handler

--- bot.py
+++ bot.py
@@ -4,9 +4,10 @@ import constants
 bot = telebot.TeleBot(constants.token)
 
 # bot.send_message(constants.chat_id, "Response test 2")
-# upd = bot.get_updates()
+upd = bot.get_updates()
 # last_upd = upd[-1]
-print(bot.get_me())
+# print(last_upd.message.message_id)
+# print(bot.get_me())
 
 def log(message, answer):
     print("\n ------------------------")
@@ -17,6 +18,19 @@ def log(message, answer):
                                                                    str(message.from_user.id),
                                                                    message.text))
     print("Ответ -", answer)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+    user_markup.row('/start', '/stop')
+    user_markup.row('фото', 'аудио', 'документы')
+    user_markup.row('стикер', 'видео', 'голос', 'локация')
+    bot.send_message(message.from_user.id, 'Добро пожаловать..', reply_markup=user_markup)
+
+@bot.message_handler(commands=['stop'])
+def handle_stop(message):
+    hide_markup = telebot.types.ReplyKeyboardRemove()
+    bot.send_message(message.from_user.id, '..', reply_markup=hide_markup)
 
 @bot.message_handler(commands=['help'])
 def handle_command(message):
@@ -35,10 +49,20 @@ def handle_text(message):
         answer = "В"
         log(message, answer)
         bot.send_message(message.from_user.id, answer)
+    elif message.text == 'фото':
+        bot.send_chat_action(message.from_user.id, 'upload_photo')
+        bot.send_photo(message.from_user.id, constants.file_not_bad)
+    # elif message.text == 'get updates':
+
+
     else:
         log(message, answer)
         bot.send_message(message.from_user.id, answer)
     # print(message)
+
+@bot.message_handler(content_types=['photo'])
+def handle_photo(message):
+    bot.send_message(message.from_user.id, message.photo.file_id)
 
 
 
