b9a78d4d288a99223b6804f29954390b6c6081bb
Maxim Garmash <maxim.garmash@gmail.com>
2019-10-10 06:19:13 +0300

    changes test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import json
 import requests
 
 
-url = 'https://api.coinmarketcap.com/v1/ticker/'
-res = requests.get(url).json()
+url = 'https://api.coinmarketcap.com/v1/ticker/start'
+res = requests.get(url).text
 print(res)
 res_json = json.dumps(res, indent=2)
 print(type(res_json))
